8798ab2dede7b8f9088daa8e021769bf149955c1
poperinA <falishaakhan05@gmail.com>
2023-10-30 10:52:42 +0800

    Matrices Q1

---
+++ Assets/Math/TestMatrix.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMatrix : MonoBehaviour
+{
+    private HMatrix2D mat = new HMatrix2D();
+    void Start()
+    {
+        mat.setIdentity();
+        mat.Print();
+    }
+
+    void Update()
+    {
+        
+    }
+}
